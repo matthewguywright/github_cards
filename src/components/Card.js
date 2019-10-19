726fae78bf9ae278456a95bf6829507e7c16d32e
Matthew Wright <1815200+matthewguywright@users.noreply.github.com>
2019-10-19 15:09:50 -0500

    Updated Card Layout
    Updated card Layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,13 +8,23 @@ class Card extends React.Component {
     render() {
         const profile = this.props;
         return (
-            <div className='card mb-2'>
-                <div className="card-body">
-                    <img className='img-thumbnail' src={profile.avatar_url} alt={profile.name} />
-                    <div className="name">{profile.name}</div>
-                    <div className="company">{profile.company}</div>
+            <div className="col-6 mb-2">
+                <div className='card'>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-4">
+                                <img className='img-fluid'  src={profile.avatar_url} alt={profile.name} />
+                            </div>
+                            <div className="col-8">
+                                <div className="name">Name: {profile.name || 'N/A'}</div>
+                                <div className="company">Company: {profile.company || 'N/A'}</div>
+                                <div className="handle">Username: <a href={profile.html_url} target='_blank'>{profile.login}</a></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         );
     }
 }
